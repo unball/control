e978cd3ac0d71a814fd9b972e6c64c02f4b6f478
sousasmurf <g.sousasmurf@gmail.com>
2017-11-06 20:45:25 -0200

    options commented

--- control_options.py
+++ control_options.py
@@ -1,7 +1,7 @@
 class control_options:
-    position = 1
-    pose = 2
-    momentum = 3
-    angular_pose= 4
-    direct_speeds = 5
-    pose_line = 6
+    position = 1		#moves to x,y position, only forward
+    pose = 2			#moves to x,y position and then turn to th, only forward
+    momentum = 3		#not implemented
+    angular_pose= 4		#turn to th
+    direct_speeds = 5	#override control actions
+    pose_line = 6		#moves to x,y position and turn to th or (th-180), go forward or backwards
